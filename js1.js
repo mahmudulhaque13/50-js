65851de5d901372e540b1dc51ceaccc50c22a1a3
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-02-09 21:06:02 +0600

    math

--- js1.js
+++ js1.js
@@ -1,7 +1,5 @@
 //  A. Math & Basic Logic (1–8)
 
-// দুটি সংখ্যার বিয়োগ, গুণ ও ভাগ বের করো
-
 // একটি সংখ্যা even না odd চেক করো
 
 // একটি সংখ্যা positive, negative না zero চেক করো
@@ -14,7 +12,7 @@
 
 // একটি সংখ্যা 3 এবং 5 উভয় দিয়ে divisible কিনা চেক করো
 
-// 1.দুটি সংখ্যা যোগ করার একটি প্রোগ্রাম লেখো
+//====== 1.দুটি সংখ্যা যোগ করার একটি প্রোগ্রাম লেখো ======
 
 // function add(a, b) {
 //   return a + b;
@@ -22,8 +20,15 @@
 // const result = add(2, 3);
 // console.log(result);
 
-const add = (a, b) => {
-  return a + b;
+// const add = (a, b) => {
+//   return a + b;
+// };
+// const result = (4, 9);
+// console.log(result);
+
+// ======= 2.দুটি সংখ্যার বিয়োগ, গুণ ও ভাগ বের করো =========
+const doMath = (x, y) => {
+  return `substruction=${x - y}, multiplication=${x * y}, divition=${x / y}`;
 };
-const result = (4, 9);
+const result = doMath(6, 2);
 console.log(result);

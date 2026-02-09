471f3a61b8544eb2451865628bea569561fe41f5
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-02-09 19:22:07 +0600

    Initial commit

---
+++ js1.js
@@ -0,0 +1,29 @@
+//  A. Math & Basic Logic (1–8)
+
+// দুটি সংখ্যার বিয়োগ, গুণ ও ভাগ বের করো
+
+// একটি সংখ্যা even না odd চেক করো
+
+// একটি সংখ্যা positive, negative না zero চেক করো
+
+// তিনটি সংখ্যার গড় (average) বের করো
+
+// Celsius থেকে Fahrenheit convert করো
+
+// Rectangle এর area বের করো
+
+// একটি সংখ্যা 3 এবং 5 উভয় দিয়ে divisible কিনা চেক করো
+
+// 1.দুটি সংখ্যা যোগ করার একটি প্রোগ্রাম লেখো
+
+// function add(a, b) {
+//   return a + b;
+// }
+// const result = add(2, 3);
+// console.log(result);
+
+const add = (a, b) => {
+  return a + b;
+};
+const result = (4, 9);
+console.log(result);

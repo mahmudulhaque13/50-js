aa3ea8bdb2c2389fb736c06fba3bd750afe09357
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-02-11 21:52:40 +0600

    leapyear check

--- conditionals.js
+++ conditionals.js
@@ -19,27 +19,35 @@ const result = bigOrSmall(101);
 console.log(result);
 
 // Student এর mark দিয়ে grade বের করো
-const grade = (mark) =>
-  typeof mark !== "number" || mark < 0 || mark > 100
-    ? "Invalid"
-    : mark >= 80
-      ? "A+"
-      : mark >= 70
-        ? "A"
-        : mark >= 60
-          ? "B"
-          : mark >= 50
-            ? "C"
-            : mark >= 40
-              ? "D"
-              : "F";
+// const grade = (mark) =>
+//   typeof mark !== "number" || mark < 0 || mark > 100
+//     ? "Invalid"
+//     : mark >= 80
+//       ? "A+"
+//       : mark >= 70
+//         ? "A"
+//         : mark >= 60
+//           ? "B"
+//           : mark >= 50
+//             ? "C"
+//             : mark >= 40
+//               ? "D"
+//               : "F";
 
-console.log(grade(85)); // A+
-console.log(grade(72)); // A
-console.log(grade(35)); // F
-console.log(grade("90")); // Invalid
+// console.log(grade(85)); // A+
+// console.log(grade(72)); // A
+// console.log(grade(35)); // F
+// console.log(grade("90")); // Invalid
 
 // Leap year চেক করার প্রোগ্রাম লেখো
+const isLeap = (year) =>
+  typeof year !== "number"
+    ? "Invalid"
+    : (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
+      ? "Leap Year"
+      : "Not Leap Year";
+
+console.log(isLeap(2024));
 
 // তিনটি সংখ্যার মধ্যে সবচেয়ে বড়টা বের করো
 

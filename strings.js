2c751b0beefdfefd005fc5a97619e2d9eae7a6a3
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-02-14 19:10:51 +0600

    word check

--- strings.js
+++ strings.js
@@ -4,13 +4,26 @@
 // console.log("String length:", message.length);
 
 // string কে uppercase করো
-const message = "hello world";
+// const message = "hello world";
 
-const upperMessage = message.toUpperCase();
+// const upperMessage = message.toUpperCase();
 
-console.log(upperMessage);
+// console.log(upperMessage);
 
 // string এর মধ্যে নির্দিষ্ট word আছে কিনা চেক করো
+// const sentence = "I love JavaScript programming";
+
+// const hasWord = sentence.includes("JavaScript");
+
+// console.log(hasWord);
+
+const sentence = "I love JavaScript programming";
+
+if (sentence.indexOf("JavaScript") !== -1) {
+  console.log("Word found!");
+} else {
+  console.log("Word not found!");
+}
 
 // একটি sentence থেকে সব word আলাদা করো
 
